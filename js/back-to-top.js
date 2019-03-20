1891e6ccdd70b0bce1b9294c9d80ac60c2d53e09
Hadrien <hboe@eleven-labs.com>
2019-03-20 14:13:55 +0100

    Add a button to go back to the top of a page

---
+++ js/back-to-top.js
@@ -0,0 +1,20 @@
+---
+layout: compress-js
+---
+(function() {
+  'use strict';
+
+  const screenSize = screen.height * 0.4;
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY >= screenSize) {
+      document.getElementById('backTop').classList.add('back-to-top--displayed');
+    } else {
+      document.getElementById('backTop').classList.remove('back-to-top--displayed');
+    }
+  });
+
+  document.getElementById('backTopLink').addEventListener('click', function() {
+    document.documentElement.scrollTop = 0;
+  });
+})();
