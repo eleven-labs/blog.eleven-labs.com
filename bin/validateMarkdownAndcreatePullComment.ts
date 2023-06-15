792e42e5c5e6bc72d403ab01417e5a0af1b00bd5
Nicolas Grévin <developer@nicolas-grevin.com>
2023-06-15 15:59:53 +0200

    fix up

--- bin/validateMarkdownAndcreatePullComment.ts
+++ bin/validateMarkdownAndcreatePullComment.ts
@@ -1,56 +1,45 @@
-import { Octokit } from "@octokit/core";
+import { Octokit } from '@octokit/core';
 import { createServer as createViteServer } from 'vite';
 
 import { MarkdownInvalidError } from '../src/helpers/markdownHelper';
-import { getArgs } from './binHelper';
-
-const repo_name = process.argv[3]
 
 const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
+  auth: process.env.GITHUB_TOKEN,
 });
 
 (async (): Promise<void> => {
-    const args = getArgs<{ ci: boolean }>();
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  });
 
-    const vite = await createViteServer({
-        server: { middlewareMode: true },
-        appType: 'custom',
+  try {
+    const { validateMarkdown } = await vite.ssrLoadModule('/src/helpers/markdownHelper.ts');
+    validateMarkdown();
+    vite.close();
+  } catch (e) {
+    vite.close();
+
+    const markdownInvalidError = e as MarkdownInvalidError;
+    const githubRef = process.env.GITHUB_REF || '';
+    const pullRequestIdMatch = Array.from(githubRef.matchAll(/refs\/pull\/(.*)\/merge/g));
+    let pullRequestId: string = '';
+    if (pullRequestIdMatch.length) pullRequestId = pullRequestIdMatch[0][1];
+
+    await octokit.request(`POST /repos/${process.env.GITHUB_REPOSITORY}/pulls/${pullRequestId}/comments`, {
+      owner: process.env.GITHUB_REPOSITORY_OWNER,
+      repo: process.env.GITHUB_REPOSITORY,
+      pull_number: pullRequestId,
+      body: markdownInvalidError.reason,
+      commit_id: process.env.GITHUB_SHA,
+      path: markdownInvalidError.markdownFilePathRelative,
+      start_line: markdownInvalidError.line,
+      start_side: 'RIGHT',
+      line: 1,
+      side: 'RIGHT',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
     });
-
-    try {
-        const { validateMarkdown } = await vite.ssrLoadModule('/src/helpers/markdownHelper.ts');
-        validateMarkdown();
-        vite.close();
-    } catch (e) {
-        vite.close();
-
-        const markdownInvalidError = e as MarkdownInvalidError;
-        // console.log(`::set-output name=filePath::${markdownInvalidError.markdownFilePathRelative}`);
-        // console.log(`::set-output name=reason::${markdownInvalidError.reason}`);
-        // console.log(`::set-output name=line::${markdownInvalidError.line}`);
-        // console.log(`::set-output name=column::${markdownInvalidError.column}`);
-
-        //
-        const github_ref = process.env.GITHUB_REF || ''
-        console.log(github_ref)
-        const pull_request_id = Array.from(github_ref.matchAll('refs\/pull\/(.*)\/merge') as any)[0][1]
-        console.log(pull_request_id)
-
-        await octokit.request(`POST /repos/${process.env.GITHUB_REPOSITORY}/pulls/${pull_request_id}/comments`, {
-            owner: process.env.GITHUB_REPOSITORY_OWNER,
-            repo: process.env.GITHUB_REPOSITORY,
-            pull_number: pull_request_id,
-            body: markdownInvalidError.reason,
-            commit_id: process.env.GITHUB_SHA,
-            path: markdownInvalidError.markdownFilePathRelative,
-            start_line: markdownInvalidError.line,
-            start_side: 'RIGHT',
-            line: 1,
-            side: 'RIGHT',
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        })
-    }
+  }
 })();
