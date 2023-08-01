e88fc6d053cb16837b779b741517dfb5954ef078
fpasquet <fpasquet@users.noreply.github.com>
2023-08-01 09:37:33 +0000

    deploy: 9b7fbc4b5830e0b37390f1a9eaac887e643852f7

---
+++ assets/Divider-6be66d5b.js
@@ -0,0 +1,2 @@
+import{j as i,U as r}from"./index.es-05e58f38.js";import{c as d}from"./index-a587463d.js";const s=({size:e="s",...a})=>i.jsx(r,{...a,as:"hr",className:d("divider",{[`divider--${e}`]:e},a.className)});try{s.displayName="Divider",s.__docgenInfo={description:"",displayName:"Divider",props:{as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"As<any>"}},size:{defaultValue:{value:"s"},description:"",name:"size",required:!1,type:{name:"enum",value:[{value:'"s"'},{value:'"m"'},{value:'"l"'}]}}}}}catch{}export{s as D};
+//# sourceMappingURL=Divider-6be66d5b.js.map
---
+++ assets/LayoutTemplateDecorator-c5295cb8.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index.es-05e58f38.js";import{L as o,a as e}from"./LayoutTemplate.stories-a3dbc530.js";const m=a=>t.jsx(o,{...e.args,children:t.jsx(a,{})});export{m as L};
+//# sourceMappingURL=LayoutTemplateDecorator-c5295cb8.js.map
