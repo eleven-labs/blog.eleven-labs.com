9371dd22097d035e8d443666ca02ddfdad14a359
fpasquet <fpasquet@users.noreply.github.com>
2023-08-17 09:13:26 +0000

    deploy: b9bcf0a182743a15575c6c7d060b93843ad3c956

---
+++ assets/BackLink-77bf64de.js
@@ -0,0 +1,2 @@
+import{j as n,l as i}from"./index.es-05e58f38.js";const a=({label:e,...r})=>n.jsx(i,{...r,icon:"arrow-back",size:"m","data-internal-link":"back",children:e});try{a.displayName="BackLink",a.__docgenInfo={description:"",displayName:"BackLink",props:{as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"As<any>"}},label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"ReactNode"}}}}}catch{}export{a as B};
+//# sourceMappingURL=BackLink-77bf64de.js.map
