c69b0fa28462b66fda03d31da20eaab68e6595c3
Eugenia <mariaeugenia.trapani.ext@eulerhermes.com>
2023-11-02 15:53:56 +0100

    add highlighted preview to PostListPage

--- src/pages/PostListPage/PostListPage.stories.tsx
+++ src/pages/PostListPage/PostListPage.stories.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { PostPreviewList, PostPreviewListProps } from '@/components';
 import NewsletterBlockStories from '@/components/NewsletterBlock/NewsletterBlock.stories';
 import * as PostPreviewListStories from '@/components/PostPreviewList/PostPreviewList.stories';
+import { ContentTypeEnum } from '@/constants';
 import { PostListPage } from '@/pages';
 
 export default {
@@ -25,6 +26,18 @@ export default {
         { label: 'Architecture' },
         { label: 'Bonnes pratiques' },
       ],
+    },
+    highlightedPostTitle: 'Notre article du moment',
+    highlightedPost: {
+      contentType: ContentTypeEnum.ARTICLE,
+      title: `Highlighted article`,
+      date: '09 fév. 2021',
+      readingTime: 24,
+      authors: [{ username: 'jdoe', name: 'J. Doe' }],
+      excerpt:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit vel tellus in molestie. Curabitur malesuada sodales consectetur. Aliquam convallis nec lacus in euismod. Vestibulum id eros vitae tellus sodales ultricies eget eu ipsum.',
+      isLoading: false,
+      isHighlighted: true,
     },
     title: 'Tous nos articles',
     postPreviewList: React.createElement<PostPreviewListProps>(PostPreviewList, {
