a7ca8af2c23e0e6d1ab9380f5f9100b0324fcbf7
fpasquet <fpasquet@users.noreply.github.com>
2023-11-02 13:06:34 +0000

    deploy: 07ee69b9002b1b987486dfb158cb513c79a185dc

---
+++ assets/LayoutTemplateDecorator-a9c60b83.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index.es-43e4d52f.js";import{L as o,a as e}from"./LayoutTemplate.stories-6360bd21.js";const m=a=>t.jsx(o,{...e.args,children:t.jsx(a,{})});export{m as L};
+//# sourceMappingURL=LayoutTemplateDecorator-a9c60b83.js.map
--- assets/LayoutTemplateDecorator-f63cfb58.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index.es-43e4d52f.js";import{L as o,a as e}from"./LayoutTemplate.stories-028a18a0.js";const m=a=>t.jsx(o,{...e.args,children:t.jsx(a,{})});export{m as L};
-//# sourceMappingURL=LayoutTemplateDecorator-f63cfb58.js.map
