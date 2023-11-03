46c0d8c915463398eb2105bad93b294df8b0776c
Charles-Eric Gorron <cgorron@eleven-labs.com>
2023-11-03 17:02:07 +0100

    fixup! fix: fix eslint warnings

--- src/containers/LinkContainer/LinkContainer.tsx
+++ src/containers/LinkContainer/LinkContainer.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Link, LinkProps, useLocation } from 'react-router-dom';
 
-export const LinkContainer = React.forwardRef<LinkProps>((props, ref) => {
+export const LinkContainer = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
   const location = useLocation();
   return <Link {...props} state={{ ...(props?.state || {}), from: location.pathname + location.search }} ref={ref} />;
 });
+
+LinkContainer.displayName = 'LinkContainer';
