114fb3b2049fd09b5734b58bd749dd2aada486e2
Pouzor <Pouzor@users.noreply.github.com>
2023-11-05 09:56:27 +0000

    deploy: 3f0040bd2e7590d1557f75538d5378f64147b93c

---
+++ assets/LayoutTemplateDecorator-9c27d536.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index.es-43e4d52f.js";import{L as o,a as e}from"./LayoutTemplate.stories-b38d141f.js";const m=a=>t.jsx(o,{...e.args,children:t.jsx(a,{})});export{m as L};
+//# sourceMappingURL=LayoutTemplateDecorator-9c27d536.js.map
