6ceb72e148aa7be553470876984b96b19a3f64d0
ElevenTom <ElevenTom@users.noreply.github.com>
2023-11-28 15:14:00 +0000

    deploy: bc8057b7e105640564e5eb701510638561e90c76

---
+++ assets/LayoutTemplateDecorator-71739f70.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index.es-43e4d52f.js";import{L as o,a as e}from"./LayoutTemplate.stories-b753021d.js";const m=a=>t.jsx(o,{...e.args,children:t.jsx(a,{})});export{m as L};
+//# sourceMappingURL=LayoutTemplateDecorator-71739f70.js.map
