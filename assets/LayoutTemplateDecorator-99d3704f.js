05b70b6dae6aa25584508aea3f29a298d82b07a1
ch3ric <ch3ric@users.noreply.github.com>
2023-12-13 17:31:09 +0000

    deploy: 2abbc1a1d05ce390fedd42778ca838a99959e25f

--- assets/LayoutTemplateDecorator-99d3704f.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index.es-43e4d52f.js";import{L as o,a as e}from"./LayoutTemplate.stories-32b8934f.js";const m=a=>t.jsx(o,{...e.args,children:t.jsx(a,{})});export{m as L};
-//# sourceMappingURL=LayoutTemplateDecorator-99d3704f.js.map
