67ea1e4a425e526b5748e3e2e959a101c1cf7cbb
fpasquet <fpasquet@users.noreply.github.com>
2024-01-04 17:54:59 +0000

    deploy: 65030708511e67ae6be7355793c7b8322c8d35ee

---
+++ assets/ArticlePage.stories-88d05338.js
@@ -0,0 +1,38 @@
+import{j as o,U as n}from"./index.es-4002838d.js";import{L as p}from"./LayoutTemplateDecorator-fb2691e2.js";import m from"./PostPage.stories-cb73e5cb.js";import"./AuthorPage-3ff73ccb.js";import"./NotFoundPage-862ae819.js";import"./SearchPage-8f2a935e.js";import{P as c}from"./PostPage-2f694774.js";import"./HomePage-0ec4a1b7.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./LayoutTemplate.stories-89b84a8f.js";import"./Header.stories-c9905669.js";import"./chunk-AY7I2SME-c7b6cf8a.js";import"./AutocompleteField.stories-46121707.js";import"./CategoryIntroBlock-a5c1e2e7.js";import"./index-e131923d.js";import"./extends-98964cd2.js";import"./index-8d47fad6.js";import"./Divider-c193e400.js";import"./ProgressBar-3379ea67.js";import"./SeparatorCircle-aa262d74.js";import"./ShareLinks-5c4238d7.js";import"./constants-8cfbdaea.js";import"./TutoTag-19d1d2c0.js";import"./NewsletterBlock-e1257afb.js";import"./assetHelper-fd0e1b85.js";import"./NotFoundBlock-f20eb071.js";import"./ContactBlock-52a83084.js";import"./HomeIntroBlock-cdd2480c.js";import"./CategoryEndingBlock-4e724064.js";import"./ContactBlock.stories-a4c9f464.js";const r=({content:e,...s})=>o.jsx(c,{...s,children:o.jsx(n,{as:"section",textSize:"s",dangerouslySetInnerHTML:{__html:e}})});try{r.displayName="ArticlePage",r.__docgenInfo={description:"",displayName:"ArticlePage",props:{contentType:{defaultValue:null,description:"",name:"contentType",required:!0,type:{name:"ContentTypeEnum.ARTICLE"}},content:{defaultValue:null,description:"",name:"content",required:!0,type:{name:"string"}},header:{defaultValue:null,description:"",name:"header",required:!0,type:{name:'Omit<PostHeaderProps, "contentType">'}},footer:{defaultValue:null,description:"",name:"footer",required:!0,type:{name:"PostFooterProps"}},contactBlock:{defaultValue:null,description:"",name:"contactBlock",required:!0,type:{name:'{ title: string; subtitle: string; description: string; link: { label: ReactNode; } & Omit<DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>, "ref">; } & FlexProps'}},relatedPostList:{defaultValue:null,description:"",name:"relatedPostList",required:!0,type:{name:"RelatedPostListProps"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}}}catch{}const U={title:"Pages/Article",component:r,args:{...m.args,content:`
+              <h1>Heading level 1</h1>
+              <h2>Heading level 2</h2>
+              <h3>Heading level 3</h3>
+              <h4>Heading level 4</h4>
+              <h5>Heading level 5</h5>
+              <h6>Heading level 6</h6>
+              <br />
+              <p>I really like using Markdown.</p>
+              <p>This is the first line.<br>And this is the second line.</p>
+              <p>I just love <strong>bold text</strong>.</p>
+              <p>Italicized text is the <em>cat's meow</em>.</p>
+              <blockquote>
+                <p>Dorothy followed her through many of the beautiful rooms in her castle.</p>
+              </blockquote>
+              <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+                <li>Fourth item</li>
+              </ul>
+              <ol>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+                <li>Fourth item</li>
+              </ol>
+              <img src="https://eleven-labs.com/static/images/planets/astro-donut.png" alt="astro donut" />
+              <a href="https://blog.eleven-labs.com/">Blog Eleven Labs</a>
+              <pre>
+                <code class="language-typescript">const hello = (name: string = 'world') => \`hello \${name} !\`;</code>
+              </pre>
+              <div class="admonition note">
+                <p class="admonition-title">Title</p>
+                Lorem ipsum
+              </div>
+        `},parameters:{layout:"full",viewport:{defaultViewport:"extraSmallScreen"}},decorators:[p]},d=e=>o.jsx(r,{...e}),t=d.bind({});var i,l,a;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:"args => <ArticlePage {...args} />",...(a=(l=t.parameters)==null?void 0:l.docs)==null?void 0:a.source}}};const $=["Overview"];export{t as Overview,$ as __namedExportsOrder,U as default};
+//# sourceMappingURL=ArticlePage.stories-88d05338.js.map
---
+++ assets/assetHelper-fd0e1b85.js
@@ -0,0 +1,2 @@
+import{B as e}from"./constants-8cfbdaea.js";const s=t=>`${e}${t.slice(1)}`;export{s as g};
+//# sourceMappingURL=assetHelper-fd0e1b85.js.map
