7cfd255d163acf21dcc2677fcc1b4a0934845371
ElevenTom <ElevenTom@users.noreply.github.com>
2024-01-10 10:14:58 +0000

    deploy: 89942a3557f5f5441415c10637367fb639332203

--- assets/preview-09e85ba5.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-7eff7f7a.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-a66f75cf.js").then(e=>e.D),["assets/DocsRenderer-3PUGWF3O-a66f75cf.js","assets/iframe-7eff7f7a.js","assets/_getPrototype-b0d2c9fe.js","assets/assert-034c52df.js","assets/index-d475d2ea.js","assets/_commonjs-dynamic-modules-302442b1.js","assets/index-d37d4223.js","assets/index-356e4a49.js"]);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-09e85ba5.js.map
