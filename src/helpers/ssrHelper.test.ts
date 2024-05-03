5fc1c14d84ad5e24c3f63fdd5a074915ef51bee0
fpasquet <fab.pasquet@gmail.com>
2024-05-03 14:44:42 +0200

    feat: upgrade all dependencies

--- src/helpers/ssrHelper.test.ts
+++ src/helpers/ssrHelper.test.ts
@@ -1,25 +1,26 @@
+import * as fs from 'node:fs';
 import { vi } from 'vitest';
 
 import { getHtmlTemplatePropsByManifest } from './ssrHelper';
 
-vi.mock('node:fs', () => ({
-  readFileSync: vi.fn().mockImplementation((path: string) => {
-    if (path === '/path/to/dir/manifest.json') {
-      return JSON.stringify({
-        'src/entry-client.tsx': {
-          css: ['styles.css'],
-          file: 'main.js',
-        },
-      });
-    }
-    if (path === '/path/to/dir/styles.css') {
-      return 'body { background: red; }';
-    }
-  }),
-}));
+vi.mock('node:fs');
 
 describe('getHtmlTemplatePropsByManifest', () => {
   it('should return the correct template props', () => {
+    vi.mocked(fs.readFileSync).mockImplementation((path: fs.PathOrFileDescriptor): string => {
+      if (path === '/path/to/dir/.vite/manifest.json') {
+        return JSON.stringify({
+          'src/entry-client.tsx': {
+            css: ['styles.css'],
+            file: 'main.js',
+          },
+        });
+      }
+      if (path === '/path/to/dir/styles.css') {
+        return 'body { background: red; }';
+      }
+      return '';
+    });
     const dirname = '/path/to/dir';
     const baseUrl = '/branch-name/';
 
